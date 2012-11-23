45a13dc195884e46b86bf102835ddcc54615cb0e
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-23 10:48:02 +0200

    Prepare taito.traviscitest 0.3.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='taito.traviscitest',
-    version='0.3.3',
+    version='0.3.4',
     description="For testing and see how Travis CI works...",
     long_description=open("README.rst").read(),
     # Get more strings from
