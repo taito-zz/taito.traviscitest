6e4be7de97514cc01f6053700d82607189705b32
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-23 11:04:12 +0200

    Removed unnecessary lined.

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setup(
     version='0.3.4',
     description="For testing and see how Travis CI works...",
     long_description=open("README.rst").read(),
-    # Get more strings from
-    # http://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
@@ -18,14 +16,13 @@ setup(
     author_email='taito.horiuchi@gmail.com',
     url='https://github.com/taito/taito.traviscitest',
     license='BSD',
-    # packages=find_packages(exclude=['ez_setup']),
     packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'': 'src'},
     namespace_packages=['taito'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Plone',
+        'Products.CMFPlone>=4.2',
         'setuptools'],
     entry_points="""
     # -*- Entry points: -*-
